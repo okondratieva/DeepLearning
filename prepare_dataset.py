2d78018648da250a143959fb1901f6203644dfa1
Ilya Hramov <hramoff.ilya@gmail.com>
2018-10-25 19:26:51 +0300

    make 'prepare_dataset.py' universal

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -6,65 +6,69 @@ def dataset(name, path):
     return {'name': name, 'path': path}
 
 file_path = os.path.dirname(__file__)
-dataset_size = 'reduce' #'origin'
-dataset_root = os.path.join(file_path, 'dataset', dataset_size)
-output_path = os.path.join(file_path, 'dataset', 'pack', dataset_size)
-
-datasets = [
-    dataset('example', os.path.join(dataset_root, 'example')),
-    dataset('train', os.path.join(dataset_root, 'train')),
-    dataset('test', os.path.join(dataset_root, 'test'))
-]
-
+dataset_root = os.path.join(file_path, 'dataset')
+output_path = os.path.join(dataset_root, 'pack')
 im2rec_path = mx.test_utils.get_im2rec_path()
 
-#example dataset preparation
-    #generate .lst file
-subprocess.check_call(
-    [
-        'python', im2rec_path,
-        '--list',
-        '--recursive',
-        '--num-thread', '4',
-        '--train-ratio', '0.8',
-        os.path.join(output_path, datasets[0]['name']),
-        datasets[0]['path']
-    ],
-    stderr=subprocess.STDOUT
-)
-    #generate binary and .idx files
-subprocess.check_call(
-    [
-        'python', im2rec_path,
-        '--num-thread', '4',
-        '--pass-through',
-        os.path.join(output_path, datasets[0]['name']),
-        datasets[0]['path']
-    ],
-    stderr=subprocess.STDOUT
-)
+dataset_types = [
+    'example',
+    'train',
+    'test'
+]
 
-#main dataset preparation
-# for dataset in datasets[1:]:
-#     subprocess.check_call(
-#         [
-#             'python', im2rec_path,
-#             '--list',
-#             '--recursive',
-#             '--num-thread', '4',
-#             os.path.join(output_path, dataset['name']),
-#             dataset['path']
-#         ],
-#         stderr=subprocess.STDOUT
-#     )
-#     #generate binary and .idx files
-#     subprocess.check_call(
-#         [
-#             'python', im2rec_path,
-#             '--num-thread', '4',
-#             '--pass-through',
-#             os.path.join(output_path, dataset['name']),
-#             dataset['path']
-#         ],
-#         stderr=subprocess.STDOUT
-#     ) 
+for size in os.listdir(dataset_root):
+    if str(size) != 'pack' and str(size) != 'origin':
+        name = dataset_types[0] + '_' + size
+
+        #example dataset preparation
+            #generate .lst file
+        subprocess.check_call(
+            [
+                'python', im2rec_path,
+                '--list',
+                '--recursive',
+                '--num-thread', '4',
+                '--train-ratio', '0.8',
+                os.path.join(output_path, name),
+                os.path.join(dataset_root, size, dataset_types[0])
+            ],
+            stderr=subprocess.STDOUT
+        )
+            #generate binary and .idx files
+        subprocess.check_call(
+            [
+                'python', im2rec_path,
+                '--num-thread', '4',
+                '--pass-through',
+                os.path.join(output_path, name),
+                os.path.join(dataset_root, size, dataset_types[0])
+            ],
+            stderr=subprocess.STDOUT
+        )
+
+        #main dataset preparation
+        for dataset_type in dataset_types[1:]:
+            name = dataset_type + '_' + size
+             #generate .lst file
+            subprocess.check_call(
+                [
+                    'python', im2rec_path,
+                    '--list',
+                    '--recursive',
+                    '--num-thread', '4',
+                    os.path.join(output_path, name),
+                os.path.join(dataset_root, size, dataset_type)
+                ],
+                stderr=subprocess.STDOUT
+            )
+            #generate binary and .idx files
+            subprocess.check_call(
+                [
+                    'python', im2rec_path,
+                    '--num-thread', '4',
+                    '--pass-through',
+                    os.path.join(output_path, name),
+                    os.path.join(dataset_root, size, dataset_type)
+                ],
+                stderr=subprocess.STDOUT
+            )
